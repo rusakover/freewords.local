3e3ed04f9ef3f6644f6101ffc67158117f0d5bfb
rusakoff <rualek@gmail.com>
2014-10-13 16:46:19 +0400

    new

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'freewords',
-			'username'  => 'root',
-			'password'  => 'root',
+			'database'  => 'rualek_freewords',
+			'username'  => 'rualek_freewords',
+			'password'  => 'befybdih2',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
